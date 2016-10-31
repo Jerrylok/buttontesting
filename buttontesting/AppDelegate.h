0bb322230b043b9f5818f730d99cc7d8a300fbb4
user <user@userteki-macbook-air.local>
2016-10-31 13:58:21 +0800

    Initial Commit

---
+++ buttontesting/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  buttontesting
+//
+//  Created by user on 31/10/2016.
+//  Copyright © 2016年 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ buttontesting/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  buttontesting
+//
+//  Created by user on 31/10/2016.
+//  Copyright © 2016年 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
